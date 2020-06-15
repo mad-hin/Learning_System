ae84ab399ab74ea9b394fcae3dc16fae2f516d93
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-06-15 23:08:46 +0800

    whatever

--- JS/system.js
+++ JS/system.js
@@ -1,22 +1,26 @@
 var extented = false;
 function collaspeSidebar() {
     if (!extented) {
-        document.getElementById("mySidebar").style.width = "175px";
-        document.getElementById("main").style.marginLeft = "175px";
+        document.getElementById("mySidebar").style.width = "180px";
         this.extented = true;
     } else {
         document.getElementById("mySidebar").style.width = "50px";
-        document.getElementById("main").style.marginLeft = "50px";
         this.extented = false;
     }
 }
+
+// Get Current HTML Page Name
+var fileName = location.pathname.split("/").slice(-1);
+console.log(fileName[0]);
 
 window.onload = function () {
     var bntHome = document.getElementById("home");
     // Action after clicking the "Home" anchor
     bntHome.onclick = function () {
         // Direct to "system.html"
-        document.location.href = 'system.html';
+        if (fileName[0] !== "system.html") {
+            document.location.href = 'system.html';
+        }
     }
     //End of Action after clicking the "New Account" anchor
     var bntNewStuAcc = document.getElementById("newStuAcc");
